39e4c968ea5eb49058e23c6ac94c727bdabcc75f
Shaun TM <cototal@outlook.com>
2025-05-26 14:06:22 -0500

    Restore some Go utilities.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'olexsmir/gopher.nvim',
+    ft = 'go',
+    build = function()
+      vim.cmd.GoInstallDeps()
+    end,
+  },
+}
--- lua/myconfig.lua
+++ lua/myconfig.lua
@@ -1,2 +1,4 @@
 vim.keymap.set({ 'n', 'v' }, ';', ':')
--- vim.opt.mouse = ''
+vim.opt.mouse = ''
+vim.keymap.set({ 'n', 'v' }, '<leader>gsj', '<cmd> GoTagAdd json <CR>', { desc = 'Add JSON struct tags' })
+vim.keymap.set({ 'n', 'v' }, '<leader>gie', '<cmd> GoIfErr <CR>', { desc = 'Insert if err block' })
