e3e41b3b1408e36d0dc731b8fe1211609ed8b598
Shaun <cototal@outlook.com>
2025-08-21 20:19:41 -0500

    Add custom snippets.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,7 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+require('luasnip.loaders.from_vscode').lazy_load { paths = { vim.fn.stdpath 'config' .. '/lua/custom/snippets' } }
 return {
   {
     'olexsmir/gopher.nvim',
@@ -9,6 +10,17 @@ return {
     build = function()
       vim.cmd.GoInstallDeps()
     end,
+  },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    -- Open parent directory in current window
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
   },
   -- {
   --   dir = vim.fn.stdpath 'config' .. '/lua/custom/plugins/codegen',
